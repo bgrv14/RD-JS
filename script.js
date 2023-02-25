0fc7f4e1dc23d155892e77ff9f9f4b3cfdc2ee4b
Your Name <you@example.com>
2023-02-25 19:25:20 +0200

    lesson21

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstNumber;
+let secondNumber;
+
+firstNumber = prompt("Enter first number");
+secondNumber = prompt("Enter second number")
+
+alert (Number(firstNumber) + Number(secondNumber));
+alert (Number(firstNumber) - Number(secondNumber));
+alert (Number(firstNumber) * Number(secondNumber));
+alert (Number(firstNumber) / Number(secondNumber));
