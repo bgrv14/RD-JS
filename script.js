82c4af9c46c0c31676de39e5e937546390b2d266
Your Name <you@example.com>
2023-03-03 18:48:31 +0200

    lesson21

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let firstNumber;
 let secondNumber;
+let result1
+let result2
+let result3
+let result4
 
 firstNumber = prompt("Enter first number");
 secondNumber = prompt("Enter second number")
 
-alert (Number(firstNumber) + Number(secondNumber));
-alert (Number(firstNumber) - Number(secondNumber));
-alert (Number(firstNumber) * Number(secondNumber));
-alert (Number(firstNumber) / Number(secondNumber));
+result1 = +firstNumber + +secondNumber;
+alert(`Сума: ${result1}`);
+
+result2 = firstNumber - secondNumber;
+alert(`Різниця: ${result2}`);
+
+result3 = firstNumber * secondNumber;
+alert(`Множення: ${result3}`);
+
+result4 = firstNumber / secondNumber;
+alert(`Ділення: ${result4}`);
